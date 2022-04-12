80c990aca932c0bc4e0e6dc0182da2dba15c7187
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-12 23:31:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
-#include <stdio.h>
-
-#include "main.h"
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
-}
-
-int main() {
+#include "test.h"
+intmain() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    return 0;
+    return0;
 }
