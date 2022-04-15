0d675aef15afa511a4cc6896c2d6ddb8d28c3829
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-15 15:17:01 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,48 @@
-#include "test.h"
-#include "common.h"
+#include <stdio.h>
+#include <assert.h>
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
 
-int numberOfMajorColors =    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+ColorPair GetColorFromPairNumber(int pairNumber) {
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
 
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+void testNumberToPair(int pairNumber,
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
 
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+void testPairToNumber(
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
+
 int main() {
-    testNumberToPair(4, enum MajorColor WHITE, enum MinorColor BROWN);
+    testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
+
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+
     return 0;
 }
