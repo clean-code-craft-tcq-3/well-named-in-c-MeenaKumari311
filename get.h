8fc53ee489969b0b1b565c7710ac919206d521a7
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-15 14:47:44 +0530

    Update get.h

--- get.h
+++ get.h
@@ -1,4 +1,4 @@
-#include "common.h"
+
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
