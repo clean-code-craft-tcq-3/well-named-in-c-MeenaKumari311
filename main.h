617e9b51f48d023d0410ecfe088b442c371cb245
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 10:21:17 +0530

    Update main.h

--- main.h
+++ main.h
@@ -15,3 +15,4 @@ const char* MinorColorNames[] = {
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
