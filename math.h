21fcf089bf884a2de960d2184da3b94ef22db0bf
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 09:53:54 +0530

    Update math.h

--- math.h
+++ math.h
@@ -12,3 +12,6 @@ int numberOfMajorColors =
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+int numberOfMinorColors =
+    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
