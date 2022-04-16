4728366b11af3f8b797943f2d40b8a2c3a036896
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 12:14:34 +0530

    Update main.h

--- main.h
+++ main.h
@@ -7,8 +7,7 @@ typedef struct
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
