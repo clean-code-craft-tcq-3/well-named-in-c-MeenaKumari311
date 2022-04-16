0f65a1ef613d9e96b9fcb98c5c8ec62ebe2d6680
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 10:04:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include "math.h"
-void ColorPairToString(const ColorPair* colorPair, char* buffer) 
-{
-    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
-}
+
 ColorPair GetColorFromPairNumber(int pairNumber) 
 {
     ColorPair colorPair;
