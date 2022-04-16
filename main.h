1c3f0c8eb096508d69a5d5429903d65836b07174
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 13:03:22 +0530

    Update main.h

--- main.h
+++ main.h
@@ -19,4 +19,5 @@ ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+void PrintWiringManual(void);
 #endif
