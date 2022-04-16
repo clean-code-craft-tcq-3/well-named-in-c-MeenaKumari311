af4ff9e72a66c75cd6ce712d57a0ad06c2007d31
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 14:16:41 +0530

    Update ManualPrinting.c

--- ManualPrinting.c
+++ ManualPrinting.c
@@ -8,7 +8,7 @@ void PrintWiringManual(void)
   {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     printf("\n PairNo : %d", pairNumber);
-    printf("Color Names %s\t", MajorColorNames[colorPair.majorColor]);
+    printf("%s \t", MajorColorNames[colorPair.majorColor]);
     printf("%s", MinorColorNames[colorPair.minorColor]);
   }
 }
