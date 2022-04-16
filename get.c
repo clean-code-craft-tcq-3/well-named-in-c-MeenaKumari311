9d3ac51a553723e30fcad0ef9396d9dd7231b959
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 12:39:29 +0530

    Update get.c

--- get.c
+++ get.c
@@ -13,3 +13,7 @@ ColorPair GetColorFromPairNumber(int pairNumber)
     colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
+int GetPairNumberFromColor(const ColorPair* colorPair) 
+{
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
+}
