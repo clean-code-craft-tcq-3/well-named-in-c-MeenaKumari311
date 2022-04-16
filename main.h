fbb9e1b517e78943a3019f6ece4af3cf5ff6ea8c
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 12:34:18 +0530

    Update main.h

--- main.h
+++ main.h
@@ -2,6 +2,11 @@
 #define __MAIN_H_
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+static char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+static int numberOfMajorColors =    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+static char* MinorColorNames[] = {    "Blue", "Orange", "Green", "Brown", "Slate"};
+static int numberOfMinorColors =    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 typedef struct 
 {
     enum MajorColor majorColor;
