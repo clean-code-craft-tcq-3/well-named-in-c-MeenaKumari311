43c5f5f17042f600c71681646b26bccd2ef2107d
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 12:34:43 +0530

    Delete get.h

--- get.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GET_H_
-#define __GET_H_
-static char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
-static int numberOfMajorColors =    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-static char* MinorColorNames[] = {    "Blue", "Orange", "Green", "Brown", "Slate"};
-static int numberOfMinorColors =    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-#endif
