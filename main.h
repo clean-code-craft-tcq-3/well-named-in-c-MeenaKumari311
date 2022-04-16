38e55f37c20038e68ff88efb8484c6e54aef6102
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 12:12:17 +0530

    Update main.h

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 #endif
