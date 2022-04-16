aead402a95a8376b0db3a3ccb223264a87baeb09
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 10:57:28 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 typedef struct 
 {
     enum MajorColor majorColor;
