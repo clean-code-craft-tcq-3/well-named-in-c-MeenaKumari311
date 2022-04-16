6453a38c92f33f9e9280ece3c7a2baa253fb2b84
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 13:26:50 +0530

    Update ManualPrinting.c

--- ManualPrinting.c
+++ ManualPrinting.c
@@ -7,7 +7,10 @@ void PrintWiringManual(void)
   for(pairNumber=0;pairNumber<25;pairNumber++)
   {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
     printf("PairNo : %d", pairNumber);
-    printf("ColorPair : %s\n", colorPair);
+    //printf("ColorPair : %s\n", colorPair);
   }
 }
