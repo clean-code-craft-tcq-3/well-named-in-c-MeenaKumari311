572f97ccfe2a754a472d29647d3cac52e7a6ee62
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 09:48:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+#include "math.h"
+// enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+// enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
