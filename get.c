98a3d186d660932477284ffb937f1fef1049e7d3
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 10:07:09 +0530

    Update get.c

--- get.c
+++ get.c
@@ -1,4 +1,5 @@
 #include "get.h"
+#include <stdio.h>
 void ColorPairToString(const ColorPair* colorPair, char* buffer) 
 {
     sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
