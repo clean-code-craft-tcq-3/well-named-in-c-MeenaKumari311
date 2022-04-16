0eb9e6dfa25c4fb3151d66684944ef4c7e1d1627
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 15:29:38 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 
 int main() 
 {
+    printf("\nTest1");
     testNumberToPair(4, WHITE, BROWN);
+    printf("\nTest2");
     testNumberToPair(5, WHITE, SLATE);
+    printf("\nTest3");
     testPairToNumber(BLACK, ORANGE, 12);
+    printf("\nTest4");
     testPairToNumber(VIOLET, SLATE, 25);
+    printf("\nReference Wiring manual:\n");
     PrintWiringManual();
     return 0;
 }
