82269dd6ad488b794f4ee283013062dea6f5e545
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 09:54:00 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 #include <assert.h>
 #include "math.h"
 
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
