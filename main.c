f41a6afbbdda952d25b914f63588922ece1d3edb
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 15:58:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 int main() 
 {
-    printf("\nTest1");
+    printf("\nTest1: Input Pair No: 4, Expected ColorPair: (WHITE,BROWN)\t");
     testNumberToPair(4, WHITE, BROWN);
-    printf("\nTest2");
+    printf("\nTest2: Input Pair No: 5, Expected ColorPair: (WHITE,SLATE)\t");
     testNumberToPair(5, WHITE, SLATE);
-    printf("\nTest3");
+    printf("\nTest3: Input  ColorPair: (BLACK,ORANGE), Expected Pair No: 12\t");
     testPairToNumber(BLACK, ORANGE, 12);
-    printf("\nTest4");
+    printf("\nTest4: Input  ColorPair: (VIOLET,SLATE), Expected Pair No: 25\t");
     testPairToNumber(VIOLET, SLATE, 25);
     printf("\nReference Wiring manual:\n");
     PrintWiringManual();
