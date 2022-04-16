01fe26cb3d29a5f125369387659d8e9f0523df98
MeenaKumari311 <100028868+MeenaKumari311@users.noreply.github.com>
2022-04-16 12:16:20 +0530

    Update main.h

--- main.h
+++ main.h
@@ -11,7 +11,8 @@ typedef struct
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 #endif
